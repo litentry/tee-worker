0453ae30af5cba71615c28ef88ce13c796d5a4b9
zhiming-zhong <zhiming.zhong@litentry.com>
2022-09-22 14:30:09 +0800

    update identity_test.sh

--- scripts/litentry/identity_test.sh
+++ scripts/litentry/identity_test.sh
@@ -30,15 +30,17 @@ cd ${root_dir}/tmp/worker1
 #${CLIENT} trusted --mrenclave ${MRENCLAVE} query-credit "//Alice"
 #echo ""
 
-# node-js:  tweet_id: Buffer.from("1571829863862116352").toJSON().data
+# node-js:  tweet_id: Buffer.from("1571829863862116352").toJSON().data.toString()
 validation_data='{"Web2":{"Twitter":{"tweet_id":[49,53,55,49,56,50,57,56,54,51,56,54,50,49,49,54,51,53,50]}}}'
-identity=''
+
+# node-js:  twitter_username: Buffer.from("litentry").toJSON().data.toString()
+identity='{"web_type":{"Web2":"Twitter"},"handle":{"String":[108,105,116,101,110,116,114,121]}}'
 
 echo "link_identity"
-RUST_LOG=warn ${CLIENT} trusted --mrenclave ${MRENCLAVE} link-identity "//Alice" "did:twitter:web2:_:myTwitterHandle"
+RUST_LOG=warn ${CLIENT} trusted --mrenclave ${MRENCLAVE} link-identity "//Alice" "$identity"
 
 echo "set-challenge-code"
-${CLIENT} trusted --mrenclave ${MRENCLAVE} set-challenge-code "//Alice" "did:twitter:web2:_:myTwitterHandle" 1134
+${CLIENT} trusted --mrenclave ${MRENCLAVE} set-challenge-code "//Alice" "$identity" 1134
 
 echo "verify-identity"
-RUST_LOG=info ${CLIENT} trusted --mrenclave ${MRENCLAVE} prepare-verify-identity "//Alice" "did:twitter:web2:_:myTwitterHandle" "$validation_data"
+RUST_LOG=info ${CLIENT} trusted --mrenclave ${MRENCLAVE} prepare-verify-identity "//Alice" "$identity" "$validation_data"
