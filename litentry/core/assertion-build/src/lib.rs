a4b5799f7e0ab58035a409b2d547dfee88b4eb5c
BillyWooo <thedreamofbilly@gmail.com>
2022-11-29 00:37:56 +0000

    clean up

--- litentry/core/assertion-build/src/lib.rs
+++ litentry/core/assertion-build/src/lib.rs
@@ -25,7 +25,6 @@ extern crate sgx_tstd as std;
 // re-export module to properly feature gate sgx and regular std environment
 #[cfg(all(not(feature = "std"), feature = "sgx"))]
 pub mod sgx_reexport_prelude {
-	// pub use chrono_sgx as chrono;
 	pub use thiserror_sgx as thiserror;
 	pub use url_sgx as url;
 }
